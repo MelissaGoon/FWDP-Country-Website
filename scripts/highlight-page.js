8c1a7690b8968391d3f3ce1f5c52a632a4094f54
Melissa Goon <melissagoon13743@gmail.com>
2025-06-18 21:23:38 -0700

    Added highlight to current page on navbar

---
+++ scripts/highlight-page.js
@@ -0,0 +1,18 @@
+const pageLinks = document.querySelectorAll("#header-menu a");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const urlArr = window.location.href.split('/');
+
+    const currPage = urlArr[urlArr.length - 1];
+
+    pageLinks.forEach((link) => {
+        if (link.id === currPage) {
+            link.classList.toggle('current-nav');
+        }
+    })
+
+});
+
+
+
